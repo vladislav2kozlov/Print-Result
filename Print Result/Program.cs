61541694bb1312703072aed9fbb2375d9b1181b7
vladislav2kozlov <vladislav2kozlov@gmail.com>
2017-03-25 11:50:49 -0700

    Updated Text

--- Print Result/Program.cs
+++ Print Result/Program.cs
@@ -11,10 +11,10 @@ namespace Print_Result
         static void Main(string[] args)
         {
             Console.WriteLine(-1 + 4 * 6);
-            Console.WriteLine((35 + 5) % 7)
+            Console.WriteLine((35 + 5) % 7);
             Console.WriteLine(14 + -4 * 6 / 11);
             Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
-
+            Console.WriteLine("Hello ther!");
             Console.ReadLine();
         }
     }
